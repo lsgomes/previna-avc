2cc5addd3a997d8eee68ee6d68d15a9112c58ebf
Lucas Dos Santos Gomes <lucas@lucass-mac.local>
2017-02-08 21:44:03 -0200

    Initial Commit

---
+++ Previna AVC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Previna AVC
+//
+//  Created by Lucas Dos Santos Gomes on 2/8/17.
+//  Copyright © 2017 Lucas Dos Santos Gomes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
